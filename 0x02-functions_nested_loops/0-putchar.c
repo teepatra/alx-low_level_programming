720d2171662f538aa15d91246eeabd951ef1d4e2
teepatra <fagboretomilola@gmail.com>
2023-02-21 08:59:38 -0800

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,23 @@
-#include <unstud.h>
+#include "main.h"
+
 /**
  * main - Entry point
  *
  * Return: Alway 0 (Sucess)
  */
 
-int _putchar(char c)
+
+int main(void)
 {
-	return (write(1, &c, 1));
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
+
