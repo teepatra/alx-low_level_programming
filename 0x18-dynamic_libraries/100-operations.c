52b8d05e12692dfeaea72ab46106aae87c8f727c
teepatra <fagboretomilola@gmail.com>
2023-06-19 06:56:10 -0700

    dynamic with c from python

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ * add - Addition function
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: The return the value. (a + b)
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - Subtraction function
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: The return the value (a - b)
+*/
+int sub(int a, int b)
+{
+		return (a - b);
+}
+
+/**
+ * mul- Multiplication function
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: The return the value (a * b)
+*/
+int mul(int a, int b)
+{
+		return (a * b);
+}
+
+/**
+ * div - Division function
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: The return the valuE (a / b)
+*/
+int div(int a, int b)
+{
+		return (a / b);
+}
+
+/**
+ * mod - Modulus function
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: The return the value (a % b)
+*/
+int mod(int a, int b)
+{
+		return (a % b);
+}
